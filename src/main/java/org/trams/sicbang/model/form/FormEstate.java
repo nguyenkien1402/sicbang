89664f580464452e58b8aac0fc6e2133581d04ff
nguyenkien1402 <nguyenkien1402@gmail.com>
2016-10-25 17:41:41 +0700

    fix api estate

--- src/main/java/org/trams/sicbang/model/form/FormEstate.java
+++ src/main/java/org/trams/sicbang/model/form/FormEstate.java
@@ -95,6 +95,8 @@ public class FormEstate extends BaseFormSearch<Estate> {
     private String premiumCostTo;
     private String isApproved;
     private String typeTrust;
+    private String userPermission;
+
 
 
     public static Specification<Estate> dongLike(final String keyword) {
@@ -154,6 +156,12 @@ public class FormEstate extends BaseFormSearch<Estate> {
                     }
                 }
                 predicates.add(criteriaBuilder.or(subPredicates.toArray(new Predicate[]{})));
+            }
+            if(!Strings.isNullOrEmpty(userPermission)){
+                System.out.println("permission :" + userPermission);
+                predicates.add(
+                        criteriaBuilder.equal(root.get(Estate_.user).get(User_.permission).get(UserPermission_.name), userPermission)
+                );
             }
             if (!Strings.isNullOrEmpty(estateType)) {
                 String[] types = estateType.split(",");
@@ -842,6 +850,14 @@ public class FormEstate extends BaseFormSearch<Estate> {
 
     public void setTypeTrust(String typeTrust) {
         this.typeTrust = typeTrust;
+    }
+
+    public String getUserPermission() {
+        return userPermission;
+    }
+
+    public void setUserPermission(String userPermission) {
+        this.userPermission = userPermission;
     }
 
     public static Estate convertEstateFormToEstate(FormEstate form, Estate estate){
